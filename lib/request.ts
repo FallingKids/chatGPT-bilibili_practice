399b088da3b388d85921eb13c9af619f47186ef8
FallingKids <739820174@qq.com>
2022-12-07 19:55:21 +0800

    action=release; project init

---
+++ lib/request.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
+
+const host = 'https://member.bilibili.com'
+
+class Request {
+  private client: AxiosInstance
+
+  constructor() {
+    this.client = axios.create({
+      baseURL: host,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  public async get(path: string): Promise<AxiosResponse> {
+    return this.client.get(path)
+  }
+
+  public async post(path: string, data: any): Promise<AxiosResponse> {
+    return this.client.post(path, data)
+  }
+}
+
+export default Request
